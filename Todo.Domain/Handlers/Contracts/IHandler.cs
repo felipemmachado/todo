0b0ca6464d9f051be7f5cb1e617f705ab3885b68
Felipe Machado <felipemmachado28@gmail.com>
2020-02-26 01:11:47 -0300

    Criando o contrado do Handler

---
+++ Todo.Domain/Handlers/Contracts/IHandler.cs
@@ -0,0 +1,11 @@
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Handlers.Contracts
+{
+    // T vai gerenciar mais de um comando, T significa generico
+    public interface IHandler<T> where T : ICommand
+    {
+        // sempre retorna um ICommandResult de um metodo Handle que recebe um comando
+        IcommandResult Handle(ICommand command);
+    }
+}
