73f2924bb27f7678c647cea3a0b2e7780520c66c
Felipe Machado <felipemmachado28@gmail.com>
2020-02-26 23:54:57 -0300

    implementando a interface do fake respository

--- Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
+++ Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Todo.Domain.Entities;
 using Todo.Domain.Repositories;
 
@@ -7,8 +9,41 @@ namespace Todo.Domain.Tests.Repositories
 
     public class FakeTodoRepository : ITodoRepository
     {
+        private List<TodoItem> _items;
+
+        public FakeTodoRepository()
+        {
+            _items = new List<TodoItem>();
+
+            _items.Add(new TodoItem("Tarefa 1", new DateTime(), "felipemachado"));
+            _items.Add(new TodoItem("Tarefa 2", new DateTime(), "felipemachado"));
+            _items.Add(new TodoItem("Tarefa 3", new DateTime(), "felipemachado"));
+            _items.Add(new TodoItem("Tarefa 1", new DateTime(), "daniellucena"));
+            _items.Add(new TodoItem("Tarefa 2", new DateTime(), "daniellucena"));
+        }
+
         public void Create(TodoItem todo)
         {
+        }
+
+        public IEnumerable<TodoItem> GetAll(string user)
+        {
+            return _items.Where(x => x.User == user);
+        }
+
+        public IEnumerable<TodoItem> GetAllDone(string user)
+        {
+            return _items.Where(x => x.User == user && x.Done);
+        }
+
+        public IEnumerable<TodoItem> GetAllPeriod(string user, DateTime date, bool done)
+        {
+            return _items.Where(x => x.User == user && x.Date.Date == date && x.Done == done);
+        }
+
+        public IEnumerable<TodoItem> GetAllUndone(string user)
+        {
+            return _items.Where(x => x.User == user && !x.Done);
         }
 
         public TodoItem GetById(Guid id, string user)
