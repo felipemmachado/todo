fe76a41b10a1d351df8458f90470bcca3dd29dfa
Felipe Machado <felipemmachado28@gmail.com>
2020-02-27 09:25:56 -0300

    criando os testes das queries

--- Todo.Domain.Tests/QueryTests/TodoQueriesTests.cs
+++ Todo.Domain.Tests/QueryTests/TodoQueriesTests.cs
@@ -16,7 +16,9 @@ namespace Todo.Domain.Tests.QueryTests
             _items = new List<TodoItem>();
 
             _items.Add(new TodoItem("Tarefa 1", new DateTime(), "felipemachado"));
-            _items.Add(new TodoItem("Tarefa 2", new DateTime(), "felipemachado"));
+            _items[0].MarkAsDone();
+
+            _items.Add(new TodoItem("Tarefa 2", new DateTime(2010, 02, 02, 00, 00, 00), "felipemachado"));
             _items.Add(new TodoItem("Tarefa 3", new DateTime(), "felipemachado"));
             _items.Add(new TodoItem("Tarefa 1", new DateTime(), "daniellucena"));
             _items.Add(new TodoItem("Tarefa 2", new DateTime(), "daniellucena"));
@@ -28,6 +30,48 @@ namespace Todo.Domain.Tests.QueryTests
             var result = _items.AsQueryable().Where(TodoQueries.GetAll("felipemachado"));
 
             Assert.True(result.Count() == 3);
+        }
+
+        [Fact]
+        public void Dada_a_consulta_deve_retonar_apenas_tarefas_completas_do_usuario_felipemachado()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAllDone("felipemachado"));
+            Assert.True(result.Count() == 1);
+        }
+
+        [Fact]
+        public void Dada_a_consulta_deve_retonar_apenas_tarefas_incompletas_do_usuario_felipemachado()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAllUndone("felipemachado"));
+            Assert.True(result.Count() == 2);
+        }
+
+        [Fact]
+        public void Dada_a_consulta_deve_retonar_tarefas_incompletas_de_um_periodo_do_usuario_felipemachado()
+        {
+            var result = _items.AsQueryable()
+                .Where(
+                    TodoQueries.GetByPeriod(
+                        "felipemachado", 
+                        new DateTime(2010, 02, 02, 00, 00, 00), 
+                        false)
+                    );
+
+          Assert.True(result.Count() == 1);
+        }
+
+        [Fact]
+        public void Dada_a_consulta_deve_retonar_tarefas_completas_de_um_periodo_do_usuario_felipemachado()
+        {
+            var result = _items.AsQueryable()
+                .Where(
+                    TodoQueries.GetByPeriod(
+                        "felipemachado", 
+                        new DateTime(2010, 02, 02, 00, 00, 00), 
+                        true)
+                    );
+
+           Assert.True(result.Count() == 0);
         }
     }
 }
